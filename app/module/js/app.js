6b72744c87fb4b509f0adf31cd91380043b500c8
Ade Imam Kurniawan <ade.imam@samsung.com>
2015-01-07 18:01:02 +0700

    fix add ng-cloak to tabs, change tab icons, change color to blue

--- app/module/js/app.js
+++ app/module/js/app.js
@@ -1,20 +1,24 @@
 (function() {
+"use strict";
 
     var app = angular.module('WebApp', ['ngMaterial']);
     app.config(['$mdThemingProvider', function($mdThemingProvider) {
         $mdThemingProvider.theme('default')
-            .primaryColor('amber')
-            .accentColor('brown');
+            .primaryColor('cyan')
+            .accentColor('indigo')
+            .backgroundColor('grey');
     }]);
 
-    app.controller('MainController', ['$scope', '$mdBottomSheet', '$mdSidenav', function($scope, $mdBottomSheet, $mdSidenav) {
-        $scope.title = 'Show Bottom Sheet';
+    app.controller('MainController', ['$scope', '$mdBottomSheet', '$mdSidenav', '$log',
+        function($scope, $mdBottomSheet, $mdSidenav, $log) {
+        var allTabs = [];
         $scope.label = {
-            showMenu: "Show Sidenav",
-            deviceInfo: "Device Info",
-            chats: "Chats",
-            contacts: "Contacts",
-            settings: "Settings"
+            showCtxMenu: 'Show Bottom Sheet',
+            showMenu: 'Show Sidenav',
+            userInfo: 'User Info',
+            tabOne: 'Tab 1',
+            tabTwo: 'Tab 2',
+            tabThree: 'Tab 3'
         };
 
         $scope.showBottomSheet = function($event) {
@@ -23,9 +27,19 @@
                 targetEvent: $event
             });
         };
+
         $scope.showMenu = function() {
             $mdSidenav('left').toggle();
         };
+
+        allTabs.push($scope.label.tabOne);
+        allTabs.push($scope.label.tabTwo);
+        allTabs.push($scope.label.tabThree);
+        $scope.$watch('mainTab.selectedIndex', function(current, old) {
+            if (!angular.isUndefined(current)) {
+                $scope.activeTab = allTabs[current];
+            }
+        });
     }]);
 
 })();
